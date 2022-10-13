117da1ed2de5af58173d92c90844c4fcb9fc47f7
Grimmyxiii <bradchundage@gmail.com>
2022-10-13 17:26:21 -0600

    Created a robot friends app

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Finley Cavalier",
+    username: "Finley",
+    email: "Finley.chundage@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Jesse Cavalier",
+    username: "Jesse",
+    email: "Jesse.chundage@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Alison Girlington",
+    username: "Alison",
+    email: "Alison.Girlington@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Bared Jattle",
+    username: "bared",
+    email: "bared.jattle@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Layla Beagle",
+    username: "Layla",
+    email: "Layla.clundage@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Brim Kiundage",
+    username: "Brim",
+    email: "Brim.Kiundage@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Boy Rundage",
+    username: "Boy",
+    email: "Boy.Rundage@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Dack Javis",
+    username: "Dack",
+    email: "Dack.Javis@gmail.com",
+  },
+  {
+    id: 9,
+    name: "Myla Woodle",
+    username: "Myla",
+    email: "Myla.alyington@gmail.com",
+  },
+  {
+    id: 10,
+    name: "P-hoe-be Westie",
+    username: "P-hoe-be",
+    email: "P.hoe.be.Chundage@gmail.com",
+  },
+  {
+    id: 11,
+    name: "Bray Clundage",
+    username: "Bray",
+    email: "Bray.Clundage@gmail.com",
+  },
+  {
+    id: 12,
+    name: "Milo Terrier",
+    username: "Milo",
+    email: "Milo.Jattle@gmail.com",
+  },
+];
